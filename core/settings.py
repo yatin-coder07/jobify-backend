6d45ac6b1cd38fa5a4812ed146f21150d91ff40c
yatin sharma <yatins113@gmail.com>
2026-01-19 12:53:05 +0530

    fix cors for frontend

--- core/settings.py
+++ core/settings.py
@@ -48,10 +48,11 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
+   
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+     'django.middleware.security.SecurityMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -151,5 +152,8 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
    "https://jobify-frontend-nine.vercel.app/"
 ]
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
